1885cd374b8457db7b1fc9624fca06db4d5340cb
Nikita Kozhevnikov <nkozhevnikov@outlook.com>
2021-11-17 21:12:48 +0700

    Added output redirection for command step

--- internal/steps/command_step.go
+++ internal/steps/command_step.go
@@ -2,6 +2,8 @@ package steps
 
 import (
 	"github.com/google/shlex"
+	"io"
+	"log"
 	"os/exec"
 )
 
@@ -17,11 +19,17 @@ func (step CommandStep) Execute() error {
 	// TODO:
 	// 1. remove usage of Windows' cmd - it is not cross-platform
 	// 2. interpret command as command block or script
+	// 3. introduce a better way of output redirection
 
 	cmdSplit, _ := shlex.Split(step.Command)
 
 	cmd := exec.Command("cmd", "/c")
 	cmd.Args = append(cmd.Args, cmdSplit...)
+
+	mw := io.MultiWriter(log.Writer())
+	cmd.Stdout = mw
+	cmd.Stderr = mw
+
 	cmdErr := cmd.Run()
 
 	return cmdErr
