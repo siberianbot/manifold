c62db169b6b49c1ca3dd41833c8110f42d913f34
Nikita Kozhevnikov <nkozhevnikov@outlook.com>
2021-11-22 15:43:13 +0700

    Added validation context factory

--- internal/validation/context.go
+++ internal/validation/context.go
@@ -1,5 +1,15 @@
 package validation
 
+import "path/filepath"
+
 type Context interface {
 	Dir() string
+}
+
+type defaultContext struct {
+	path string
+}
+
+func (d *defaultContext) Dir() string {
+	return filepath.Dir(d.path)
 }
