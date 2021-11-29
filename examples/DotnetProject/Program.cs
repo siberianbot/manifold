057b768aa7a1d49d0577aeb265291d159fa56cf0
Nikita Kozhevnikov <nkozhevnikov@outlook.com>
2021-11-29 18:40:13 +0700

    Added .NET project example

---
+++ examples/DotnetProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
